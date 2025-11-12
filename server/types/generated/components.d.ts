507d00da918cd27dcc9e2d55f1634afe58495971
amirnasr94 <amir.nasr72@gmail.com>
2025-11-13 01:14:56 +0330

    add blocks

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,75 +1,60 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
-export interface SharedMedia extends Struct.ComponentSchema {
-  collectionName: 'components_shared_media';
+export interface BlocksHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_hero_sections';
   info: {
-    displayName: 'Media';
-    icon: 'file-video';
+    displayName: 'Hero Section';
   };
   attributes: {
-    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    author: Schema.Attribute.String;
+    avatar: Schema.Attribute.Media<'images'>;
+    cta: Schema.Attribute.Component<'elements.link', false>;
+    heading: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images'>;
+    lable: Schema.Attribute.Enumeration<['technology', 'health']>;
+    publishDate: Schema.Attribute.Date;
   };
 }
 
-export interface SharedQuote extends Struct.ComponentSchema {
-  collectionName: 'components_shared_quotes';
+export interface BlocksLatestBlogs extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_latest_blogs';
   info: {
-    displayName: 'Quote';
-    icon: 'indent';
+    displayName: 'latest blogs';
+  };
+  attributes: {};
+}
+
+export interface ElementsLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_links';
+  info: {
+    displayName: 'Link';
   };
   attributes: {
-    body: Schema.Attribute.Text;
-    title: Schema.Attribute.String;
+    disable: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    href: Schema.Attribute.Text;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
   };
 }
 
-export interface SharedRichText extends Struct.ComponentSchema {
-  collectionName: 'components_shared_rich_texts';
+export interface ElementsLogo extends Struct.ComponentSchema {
+  collectionName: 'components_elements_logos';
   info: {
-    description: '';
-    displayName: 'Rich text';
-    icon: 'align-justify';
+    displayName: 'Logo';
   };
   attributes: {
-    body: Schema.Attribute.RichText;
-  };
-}
-
-export interface SharedSeo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_seos';
-  info: {
-    description: '';
-    displayName: 'Seo';
-    icon: 'allergies';
-    name: 'Seo';
-  };
-  attributes: {
-    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
-    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
-    shareImage: Schema.Attribute.Media<'images'>;
-  };
-}
-
-export interface SharedSlider extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sliders';
-  info: {
-    description: '';
-    displayName: 'Slider';
-    icon: 'address-book';
-  };
-  attributes: {
-    files: Schema.Attribute.Media<'images', true>;
+    alt: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'shared.media': SharedMedia;
-      'shared.quote': SharedQuote;
-      'shared.rich-text': SharedRichText;
-      'shared.seo': SharedSeo;
-      'shared.slider': SharedSlider;
+      'blocks.hero-section': BlocksHeroSection;
+      'blocks.latest-blogs': BlocksLatestBlogs;
+      'elements.link': ElementsLink;
+      'elements.logo': ElementsLogo;
     }
   }
 }
